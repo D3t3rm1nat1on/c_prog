5ce95fbe8637accbc253e8a68f6a4d1869a666cb
Andrey Chekushin <send_dudes>
2023-06-07 00:49:32 +0300

    build.mk --> added build_root rule dep

--- build.mk
+++ build.mk
@@ -6,13 +6,13 @@ define test_rule
 -include $($1_src_objs:.o=.d)
 -include $(deps_dir)/$1.d
 
-$(build_root)/%.o: $$($1_src_root)/%.c $(deps_dir)/%.d | $(deps_dir)
+$(build_root)/%.o: $$($1_src_root)/%.c $(deps_dir)/%.d | $(deps_dir) $(build_root)
 	$(CC) $(CFLAGS) -c -o $$@ $$<
 
-$(build_root)/%.dump: (build_root)/%.o
+$(build_root)/%.dump: (build_root)/%.o | $(build_root)
 	objdump -Dz $$< > $$@
 
-$(build_root)/$1: $(build_root)/$1.o $$($1_src_objs) $(lib_objs) 
+$(build_root)/$1: $(build_root)/$1.o $$($1_src_objs) $(lib_objs) | $(build_root)
 	$(CC) $(CFLAGS) -o $$@ $$^
 
 $1: $(build_root)/$1 | $(build_root)
